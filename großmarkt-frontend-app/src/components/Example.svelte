8aebd56bbc074ebc059e50a827b3714b88aa4408
Nils Malte Teichert <nils.teichert@ibm.com>
2021-04-17 20:40:56 +0200

    add svelte boilerplate

---
+++ großmarkt-frontend-app/src/components/Example.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<main>
+    <h1>Hello, World.</h1>
+    <p>This is an example component.</p>
+</main>
+
+<style>
+    p{
+        font: 1rem "Comic Sans MS";
+    }
+</style>
