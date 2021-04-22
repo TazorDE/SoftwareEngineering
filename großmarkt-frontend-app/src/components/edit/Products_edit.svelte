0fee5af411084671c0a198430421c7790a956c93
TazorDE <malteteichert@web.de>
2021-04-22 16:33:36 +0200

    add comments

--- großmarkt-frontend-app/src/components/edit/Products_edit.svelte
+++ großmarkt-frontend-app/src/components/edit/Products_edit.svelte
@@ -1,6 +1,5 @@
 <script>
-import { text } from "svelte/internal";
-
+    //set up props for use in the component
     export let id;
     export let name;
     export let verkaufspreis;
@@ -59,64 +58,64 @@ import { text } from "svelte/internal";
             <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
         </svg>
     </button>
-    <form>
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h3 class="modal-title" id="exampleModalLabel">
-                            Eintrag bearbeiten
-                        </h3>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <!-- form for editing an existing produkt. filled with relevant prop data -->
-                        <label for="produktname">Produktname</label>
-                        <input type="text" class="form-control" id="produktname" bind:value="{name}"/>
-                        <label for="anzahl">Anzahl</label>
-                        <input type="text" class="form-control" id="anzahl" bind:value="{anzahl}"/>
-                        <label for="herkunft">Herkunft</label>
-                        <select class="form-control" id="herkunft" bind:value="{herkunft}">
-                            <option>Deutschland</option>
-                            <option>Spanien</option>
-                            <option>Neuseeland</option>
-                            <option>Mond</option>
-                            <option>Frankreich</option>
-                        </select>
-                        <label for="kategorie">Kategorie</label>
-                        <select class="form-control" id="kategorie" bind:value="{kategorie}">
-                            <option>Kernobst</option>
-                            <option>Steinobst</option>
-                            <option>Beerenobst</option>
-                            <option>Schalenobst</option>
-                            <option>Südfrüchte / exotische Früchte</option>
-                            <option>Blattgemüse</option>
-                            <option>Fruchtgemüse</option>
-                            <option>Sprossengemüse</option>
-                            <option>Wintergemüse</option>
-                            <option>Wurzelgemüse</option>
-                            <option>Hülsenfrüchte</option>
-                            <option>Sonstige</option>
-                        </select>
-                        <label for="verkaufspreis">Verkaufspreis</label>
-                        <input type="text" class="form-control" id="verkaufspreis" bind:value="{verkaufspreis}"/>
-                        <label for="einkaufspreis">Einkaufspreis</label>
-                        <input type="text" class="form-control" id="einkaufspreis" bind:value="{einkaufspreis}"/>
-                        <label for="bezugsquelle">Bezugsquelle</label>
-                        <input type="text" class="form-control" id="bezugsquelle" bind:value="{bezugsquelle}"/>
-                        <label for="mhd">Mindesthaltbarkeitsdatum</label>
-                        <input type="date" class="form-control" id="mhd" bind:value="{mhd}"/>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Abbruch</button>
-                        <button type="button" class="btn btn-primary" on:click={update_database}>Daten überschreiben</button>
-                    </div>
+    
+    <!-- Modal used to edit existing product data -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title" id="exampleModalLabel">
+                        Eintrag bearbeiten
+                    </h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <!-- form for editing an existing produkt. filled with relevant prop data -->
+                    <label for="produktname">Produktname</label>
+                    <input type="text" class="form-control" id="produktname" bind:value="{name}"/>
+                    <label for="anzahl">Anzahl</label>
+                    <input type="text" class="form-control" id="anzahl" bind:value="{anzahl}"/>
+                    <label for="herkunft">Herkunft</label>
+                    <select class="form-control" id="herkunft" bind:value="{herkunft}">
+                        <option>Deutschland</option>
+                        <option>Spanien</option>
+                        <option>Neuseeland</option>
+                        <option>Mond</option>
+                        <option>Frankreich</option>
+                    </select>
+                    <label for="kategorie">Kategorie</label>
+                    <select class="form-control" id="kategorie" bind:value="{kategorie}">
+                        <option>Kernobst</option>
+                        <option>Steinobst</option>
+                        <option>Beerenobst</option>
+                        <option>Schalenobst</option>
+                        <option>Südfrüchte / exotische Früchte</option>
+                        <option>Blattgemüse</option>
+                        <option>Fruchtgemüse</option>
+                        <option>Sprossengemüse</option>
+                        <option>Wintergemüse</option>
+                        <option>Wurzelgemüse</option>
+                        <option>Hülsenfrüchte</option>
+                        <option>Sonstige</option>
+                    </select>
+                    <label for="verkaufspreis">Verkaufspreis</label>
+                    <input type="text" class="form-control" id="verkaufspreis" bind:value="{verkaufspreis}"/>
+                    <label for="einkaufspreis">Einkaufspreis</label>
+                    <input type="text" class="form-control" id="einkaufspreis" bind:value="{einkaufspreis}"/>
+                    <label for="bezugsquelle">Bezugsquelle</label>
+                    <input type="text" class="form-control" id="bezugsquelle" bind:value="{bezugsquelle}"/>
+                    <label for="mhd">Mindesthaltbarkeitsdatum</label>
+                    <input type="date" class="form-control" id="mhd" bind:value="{mhd}"/>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Abbruch</button>
+                    <button type="button" class="btn btn-primary" on:click={update_database}>Daten überschreiben</button>
                 </div>
             </div>
         </div>
-    </form>
+    </div>
 </main>
 
 <style>
